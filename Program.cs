0edcb35f7259cbfe5f31d9d1489f4c345a8300fc
HassibKareme <96119570+HassibKareme@users.noreply.github.com>
2022-12-07 14:38:25 +0100

    initialCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubActions_Oef1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
